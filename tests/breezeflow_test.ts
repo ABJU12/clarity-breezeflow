003b17270db974fb5c3f846f39684375a91bcab9
ABJU12 <auduabdulrauf320@gmail.com>
2025-02-03 21:52:24 +0100

    test: Add initial contract tests

---
+++ tests/breezeflow_test.ts
@@ -0,0 +1,48 @@
+import {
+  Clarinet,
+  Tx,
+  Chain,
+  Account,
+  types
+} from 'https://deno.land/x/clarinet@v1.0.0/index.ts';
+import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
+
+Clarinet.test({
+  name: "Ensure can submit new activity",
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    const wallet_1 = accounts.get("wallet_1")!;
+    
+    let block = chain.mineBlock([
+      Tx.contractCall(
+        "breezeflow",
+        "submit-activity",
+        ["sunny", "beach volleyball"],
+        wallet_1.address
+      )
+    ]);
+    
+    assertEquals(block.receipts.length, 1);
+    assertEquals(block.height, 2);
+    block.receipts[0].result.expectOk().expectUint(1);
+  },
+});
+
+Clarinet.test({
+  name: "Ensure can vote on activity only once",
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    const wallet_1 = accounts.get("wallet_1")!;
+    
+    let block = chain.mineBlock([
+      Tx.contractCall(
+        "breezeflow",
+        "vote-activity",
+        [types.uint(1)],
+        wallet_1.address
+      )
+    ]);
+    
+    assertEquals(block.receipts.length, 1);
+    assertEquals(block.height, 2);
+    block.receipts[0].result.expectOk().expectBool(true);
+  },
+});
